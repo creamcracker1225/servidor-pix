24fc89d79792e98e040a239f11eff80320b10e08
creamcracker1225 <brunnnnnacopperfield1225@gmail.com>
2026-02-16 01:02:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mercadopago.configurations.setAccessToken(APP_USR-8792764793047739-021523-d3f366e01d1f05bdab52560161304b9d-98956509);
+mercadopago.configurations.setAccessToken("APP_USR-8792764793047739-021523-d3f366e01d1f05bdab52560161304b9d-98956509");
 
 app.post("/criar-pix", async (req, res) => {
   try {
